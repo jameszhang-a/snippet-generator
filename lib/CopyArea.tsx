a7e477839adedfb693d3154cb7dd7688d1462c7a
James Zhang <jameszhanga@gmail.com>
2022-03-18 02:47:54 -0400

    Actually able to parse from output now

--- lib/CopyArea.tsx
+++ lib/CopyArea.tsx
@@ -1,5 +1,7 @@
-import { Textarea } from '@mantine/core';
-import React from 'react';
+import { MediaQuery, Textarea } from '@mantine/core';
+import React, { useContext, useState } from 'react';
+import SnippetProvider, { Snippet } from '../Contexts/SnippetProvider';
+import { AreaInputEvent } from './Translation';
 
 type Props = {
   target: string;
@@ -8,17 +10,35 @@ type Props = {
 };
 
 const CopyArea = ({ target, setTarget, handleChange }: Props) => {
+  const { snippet, setSnippet } = useContext(SnippetProvider.context);
+  const [ outputText, setOutputText ] = useState('');
+
+  const handleOutputChange = (e: AreaInputEvent) => {
+    const value = e.target.value;
+
+    // Turn target into JS object
+    const raw = JSON.parse(value);
+    // Grab the 1 and only key as the name
+    const name = Object.keys(raw)[0];
+    // Finally construct the Snippet object
+    const parsed: Snippet = { name, ...raw[name] };
+
+    setSnippet(parsed);
+    setOutputText(value);
+  };
+
   return (
-    <React.Fragment>
+    <MediaQuery largerThan='md' styles={{ width: '50vw' }}>
       <Textarea
         placeholder='output'
         variant='default'
+        sx={{ width: '80vw' }}
         autosize
-        value={target}
-        onChange={handleChange}
+        value={outputText}
+        onChange={(e) => handleOutputChange(e)}
         minRows={5}
       />
-    </React.Fragment>
+    </MediaQuery>
   );
 };
 
