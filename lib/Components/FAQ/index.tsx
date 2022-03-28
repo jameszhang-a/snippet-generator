6338b9bdab243f87533017ab5e60d3dfa3e62d74
James Zhang <jameszhanga@gmail.com>
2022-03-28 16:23:00 -0500

    Finish what is a snippet section

--- lib/Components/FAQ/index.tsx
+++ lib/Components/FAQ/index.tsx
@@ -1,15 +1,50 @@
-import { Accordion, MediaQuery } from '@mantine/core';
-import { faqInfo } from './faqInfo';
+import { Accordion, Anchor, Code, MediaQuery, Space, Text } from '@mantine/core';
+import Image from 'next/image';
 
 const FAQ = () => {
   return (
     <MediaQuery largerThan='md' styles={{ width: '50vw' }}>
       <Accordion multiple sx={{ width: '80vw', paddingTop: '20px' }}>
-        {faqInfo.map((item: any) => (
-          <Accordion.Item key={item.title} label={item.title}>
-            {item.text}
-          </Accordion.Item>
-        ))}
+        <Accordion.Item label='What is a Snippet?'>
+          <Text>
+            Code snippets are templates that make it easier to enter repeating code
+            patterns, such as loops or conditional-statements.
+          </Text>
+          <Space h='md' />
+          <Text>
+            In Visual Studio Code, snippets appear in IntelliSense (<Code>⌃Space</Code>)
+            mixed with other suggestions, as well as in a dedicated snippet picker (
+            <Text component='span' weight={700}>
+              Insert Snippet{' '}
+            </Text>
+            in the Command Palette). There is also support for tab-completion: Enable it
+            with <Code>&quot;editor.tabCompletion&quot;: &quot;on&quot;</Code>, type a
+            <Text component='span' weight={700}>
+              {' '}
+              snippet prefix (trigger text)
+            </Text>, and press <Code>Tab</Code> to insert a snippet.
+          </Text>
+          <Space h='md' />
+          <Image
+            alt='Snippet example'
+            src='/snippet_example.gif'
+            quality={75}
+            layout='responsive'
+            width={800}
+            height={450}
+          />
+          <Text>
+            For more information, visit{' '}
+            <Anchor
+              href='https://code.visualstudio.com/docs/editor/userdefinedsnippets'
+              target='_blank'
+            >
+              the vscode docs
+            </Anchor>.
+          </Text>
+        </Accordion.Item>
+
+        <Accordion.Item label='How to use Snippet Builder'>hello</Accordion.Item>
       </Accordion>
     </MediaQuery>
   );
